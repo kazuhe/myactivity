dede2b528875feebf2fa00e913918e4f3a798e9c
kazuhe <omc.ohara@gmail.com>
2021-12-06 01:10:10 +0900

    VUE CLI -> Vite

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import VueCompositionAPI from '@vue/composition-api'
 
-Vue.config.productionTip = false
-
-Vue.use(VueCompositionAPI)
-
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
